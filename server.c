9fd72bbae97c0ab460d37c46fb21500b950316ee
Alexander Ameye <alexanderameye@gmail.com>
2019-11-02 14:54:33 +0900

    Fixed tons

--- server.c
+++ server.c
@@ -5,6 +5,15 @@
 #include <netinet/in.h>
 #include <string.h>
 #include "common.h"
+#include <pthread.h>
+
+//TODO: send html files
+//TODO: send not on same wifi working
+//TODO: multithreading support
+//TODO: login
+//TODO: jpg files
+
+void *connection_handler(void *);
 
 /* For example http://localhost:10080/alex.html
  * url turned into request message
@@ -12,152 +21,6 @@
  * Host: localhost:10080
  *
  * */
-
-void die(char *message, ...);
-void live(char *message, ...);
-
-int main(int argc, char const *argv[]) {
-    /* VARIABLES */
-    int server_socket, client_socket;
-    long value_read, value_write;
-    struct sockaddr_in server_address;
-    int address_length = sizeof(server_address);
-    int send_bytes;
-    char request[BUFFER_SIZE];
-    char response[BUFFER_SIZE];
-    char *server_response = "No message";
-
-    /* CREATE SERVER SOCKET */
-    /* creates a server socket */
-    if ((server_socket = socket(SOCKET_DOMAIN, SOCKET_TYPE, SOCKET_PROTOCOL)) == -1)
-        die("Socket creation failed");
-    else live("Socket created");
-
-    server_address.sin_family = SOCKET_DOMAIN;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(SERVER_PORT);
-
-    memset(server_address.sin_zero, '\0', sizeof server_address.sin_zero);
-
-    /* BIND */
-    /* assigns port number to server socket */
-    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1)
-        die("Socket bind failed");
-    else live("Socket bound");
-
-    /* LISTEN */
-    if (listen(server_socket, MAX_CONNECTIONS) == -1) die("Socket listen failed");
-
-
-    while (1) {
-        live("Socket listening...");
-        if ((client_socket = accept(server_socket, (struct sockaddr *) &server_address,
-                                    (socklen_t * ) & address_length)) == -1)
-            die("Socket accept failed");
-        else live("Client socket accepted");
-
-        /* READ CLIENT REQUEST */
-        if ((value_read = read(client_socket, request, BUFFER_SIZE)) >= 0) {
-            live("Client request received:\n%s%s%s\n", COLOR_CLIENT_CONTENT, request,
-                 COLOR_NEUTRAL);
-
-            /* GET REQUESTED FILE */
-            const char *start_of_path = strchr(request, '/') + 1;
-            const char *end_of_path = strchr(request, 'H') - 1;
-            char path[end_of_path - start_of_path];
-            strncpy(path, start_of_path, end_of_path - start_of_path);
-            path[sizeof(path)] = 0;
-            live("Requested file:\n%s%s%s\n", COLOR_CLIENT_CONTENT, path, COLOR_NEUTRAL);
-
-            /* CREATE HEADERS BASED ON FILE TYPE */
-            char *response_header;
-            char *dot = strrchr(path, '.');
-            if (dot && !strcmp(dot, ".html")) {
-                response_header =
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: text/html\r\n\n";
-
-                FILE *file;
-                char *content;
-                long numbytes;
-
-                file = fopen(path, "r");
-
-                fseek(file, 0L, SEEK_END);
-                numbytes = ftell(file); //returns current file position
-                fseek(file, 0L, SEEK_SET); //reset file position
-
-                content = (char *) calloc(numbytes, sizeof(char));
-
-                fread(content, sizeof(char), numbytes, file);
-                fclose(file);
-
-                //  char *content = "<h1>AA</h1>";
-                server_response = (char *) malloc(1 + strlen(response_header) + strlen(content));
-                strcpy(server_response, response_header);
-                strcat(server_response, content);
-
-                free(content);
-
-
-            } else if (dot && !strcmp(dot, ".jpg")) {
-                response_header =
-                        "HTTP/1.1 200 OK\r\n"
-                        "Content-Type: image/jpg\r\n\n";
-
-                FILE *file;
-                char *content;
-                long numbytes;
-
-                file = fopen(path, "r");
-
-                fseek(file, 0L, SEEK_END);
-                numbytes = ftell(file); //returns current file position
-                fseek(file, 0L, SEEK_SET); //reset file position
-
-                content = (char *) calloc(numbytes, sizeof(char));
-
-                fread(content, sizeof(char), numbytes, file);
-                fclose(file);
-
-
-                //  char *content = "<h1>AA</h1>";
-                server_response = (char *) malloc(1 + strlen(response_header) + strlen(content));
-                strcpy(server_response, response_header);
-                strcat(server_response, content);
-
-                free(content);
-
-
-            } else {
-                response_header =
-                        "HTTP/1.1 404 Not Found\r\n"
-                        "Content-Type: image/jpg\r\n\n<h1>404</h1>";
-
-            }
-
-            memset(request, 0, BUFFER_SIZE);
-        } else {
-            die("Reading client request failed");
-            close(client_socket);
-        }
-
-
-        /* WRITE SERVER RESPONSE */
-
-        send_bytes = strlen(server_response);
-
-        if (write(client_socket, server_response, send_bytes) != send_bytes)
-            die("Writing server response failed");
-        else
-            live("Server response sent:\n%s%s%s", COLOR_SERVER_CONTENT, server_response, COLOR_NEUTRAL);
-
-        close(client_socket);
-    }
-    return 0;
-}
-
-
 
 void die(char *message, ...) {
     va_list ap;
@@ -181,4 +44,117 @@ void live(char *message, ...) {
     fprintf(stdout, "\n");
     fflush(stdout);
     va_end(ap);
+}
+
+int main(int argc, char const *argv[]) {
+    /* VARIABLES */
+    int server_socket, client_socket;
+    long value_read, value_write;
+    struct sockaddr_in server_address;
+    int address_length = sizeof(server_address);
+    int send_bytes;
+    char request[BUFFER_SIZE];
+    char response[BUFFER_SIZE];
+    char *server_response_header = "HTTP/1.1 200 OK\r\n"
+                                   "Content-Type: text/html\r\n\n";
+    char *server_response;
+
+    /* CREATE SERVER SOCKET */
+    if ((server_socket = socket(SOCKET_DOMAIN, SOCKET_TYPE, SOCKET_PROTOCOL)) == -1)
+        die("Socket creation failed");
+    else live("Socket created");
+
+    server_address.sin_family = SOCKET_DOMAIN;
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(SERVER_PORT);
+
+    /* BIND */
+    /* assigns port number to server socket */
+    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) == -1)
+        die("Socket bind failed");
+    else live("Socket bound");
+
+    /* LISTEN FOR CONNECTION REQUESTS */
+    if (listen(server_socket, MAX_CONNECTIONS) == -1) die("Socket listen failed");
+
+    while (1) {
+        live("Socket listening...");
+
+        /* ACCEPT CONNECTION REQUESTS */
+        if ((client_socket = accept(server_socket, (struct sockaddr *) &server_address,
+                                    (socklen_t * ) & address_length)) == -1)
+            die("Socket accept failed");
+        else live("Client socket accepted");
+
+        /* READ CLIENT REQUEST */
+        if ((value_read = read(client_socket, request, BUFFER_SIZE)) >= 0) {
+            live("Client request received:\n%s%s%s\n", COLOR_CLIENT_CONTENT, request,
+                 COLOR_NEUTRAL);
+
+            /* GET REQUESTED FILE */
+            const char *start_of_path = strchr(request, '/') + 1;
+            const char *end_of_path = strchr(request, 'H') - 1;
+            char path[end_of_path - start_of_path];
+            strncpy(path, start_of_path, end_of_path - start_of_path);
+            path[sizeof(path)] = 0;
+            live("Requested file:\n%s%s%s\n", COLOR_CLIENT_CONTENT, path, COLOR_NEUTRAL);
+
+            /* CREATE HEADERS BASED ON FILE TYPE */
+            char *dot = strrchr(path, '.');
+            if (dot && !strcmp(dot, ".html")) { //HTML
+                server_response_header =
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: text/html\r\n\n";
+            } else if (dot && !strcmp(dot, ".jpg")) { //JPG
+                server_response_header =
+                        "HTTP/1.1 200 OK\r\n"
+                        "Content-Type: image/jpg\r\n\n";
+
+                live("RESPONSE: \n%s%s%s\n", COLOR_CLIENT_CONTENT, server_response_header, COLOR_NEUTRAL);
+            }
+            /* READ FILE CONTENTS */
+            FILE *file = fopen(path, "rb");
+
+            if (file != NULL) {
+                fseek(file, 0, SEEK_END);
+                long file_size = ftell(file);
+                rewind(file);
+
+                char *file_content = (char *) malloc(file_size + 1);
+                fread(file_content, 1, file_size, file);
+                file_content[file_size] = 0;
+
+                fclose(file);
+
+                printf("%s", file_content);
+
+                /* ADD HEADER + BODY TO RESPONSE */
+                server_response = malloc(strlen(server_response_header) + strlen(file_content) + 1);
+                strcpy(server_response, server_response_header);
+                strcat(server_response, file_content);
+
+                free(file_content);
+
+                /* WRITE SERVER RESPONSE */
+                send_bytes = strlen(server_response);
+                if (write(client_socket, server_response, send_bytes) != send_bytes)
+                    die("Writing server response failed");
+                else
+                    live("Server response sent:\n%s%s%s", COLOR_SERVER_CONTENT, server_response, COLOR_NEUTRAL);
+
+                free(server_response);
+                close(client_socket);
+
+            } else {
+                printf("%s[SERVER] ", COLOR_NEGATIVE);
+                printf("%s%s\n", COLOR_NEUTRAL, "No such file or directory, skipped");
+                close(client_socket);
+            }
+
+        } else {
+            die("Reading client request failed");
+            close(client_socket);
+        }
+    }
+    return 0;
 }
