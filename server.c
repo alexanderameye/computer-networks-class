d114f24e852742bf129c2bcbc706b3655308f575
Alexander Ameye <alexanderameye@gmail.com>
2019-11-02 19:11:03 +0900

    Update server.c

--- server.c
+++ server.c
@@ -8,11 +8,11 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <fcntl.h>
-#include <sys/stat.h>
+
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int get_file_size(int);
+#define HTML404 "<html><head> <title>404 Not Found</title> <style> html{ height: 100%; } body{ font-family: 'Segoe UI', sans-serif;; height: 100%; } .wrap { width: 100%; height: 100%; display: flex; justify-content: center; align-items: center; background: #fafafa; } .login-form{ border: 1px solid #ddd; padding: 2rem; background: #ffffff; } .form-button{ background: #d95b52; border: 1px solid #ddd; color: #ffffff; padding: 10px; width: 100%;font-weight:bold; } .form-header{ text-align: center; margin-bottom : 1rem; } </style></head><body> <div class=\"wrap\"> <form class=\"login-form\"> <div class=\"form-header\"> <h3>404 Not Found</h3> </div> <button class=\"form-button\" onclick=\"location.href='/index.html'\" type=\"button\">Go Back</button> </form> </div></body></html>\r\n"
 
 void *handle_connection(void *);
 
@@ -38,7 +38,8 @@ int main(int argc, char const *argv[]) {
     if (listen(server_socket, MAX_CONNECTIONS) == -1) die("Socket listen failed");
 
     while (1) {
-        live("Socket listening...\n");
+        live("%sSocket listening...%s\n", COLOR_SERVER_CONTENT, COLOR_NEUTRAL);
+
         socklen_t sin_size = sizeof(struct sockaddr_in);
 
         /* ACCEPT CONNECTION REQUESTS */
@@ -60,140 +61,52 @@ int main(int argc, char const *argv[]) {
 
 void *handle_connection(void *client_socket) {
     int socket = *(int *) client_socket;
-    int send_bytes;
-    long value_read, value_write;
-    char *server_response_header = "HTTP/1.1 200 OK\r\n"
-                                   "Content-Type: text/html\r\n\n";
-    char *server_response;
-
     int requested_file;
-    unsigned char request[BUFFER_SIZE], resource[BUFFER_SIZE];
-    int length = recv_line(socket, request);
-    unsigned char *ptr; //used to traverse the request line string
 
-    ptr = strstr(request, " HTTP/");//search for valid looking request
-    if (ptr == NULL) printf(" NOT HTTP!\n");
+    /* READ REQUEST */
+    unsigned char request[BUFFER_SIZE];
+    int file_size = recv_line(socket, request);
+    live("Client request received:\n%s\t %s%s\n", COLOR_POSITIVE, request,
+         COLOR_NEUTRAL);
+
+    /* PARSE REQUEST */
+    unsigned char *ptr; // used to traverse the request
+    unsigned char file_path[BUFFER_SIZE];
+
+    ptr = strstr(request, " HTTP/"); // find first occurrence
+    if (ptr == NULL) die("Request was not HTTP");
     else {
-        *ptr = 0; // terminate the buffer at the end of the URL
-        ptr = NULL; // set ptr to NULL (used to flag for an invalid request)
-        if (strncmp(request, "GET ", 4) == 0)  // get request
-            ptr = request + 4; // ptr is the URL
-        if (strncmp(request, "HEAD ", 5) == 0) // head request
-            ptr = request + 5; // ptr is the URL
-
-        if (ptr == NULL) { // then this is not a recognized request
-            printf("\tUNKNOWN REQUEST!\n");
-        } else { // valid request, with ptr pointing to the resource name
-            if (ptr[strlen(ptr) - 1] == '/')  // for resources ending with '/'
-                strcat(ptr, "index.html");     // add 'index.html' to the end
-            strcpy(resource, WEBROOT);     // begin resource with web root path
-            strcat(resource, ptr);         //  and join it with resource path
-            requested_file = open(resource, O_RDONLY, 0); // try to open the file
-            printf("\tOpening \'%s\'\t", resource);
-            if (requested_file == -1) { // if file is not found
-                printf(" 404 Not Found\n");
-                send_string(socket, "HTTP/1.0 404 NOT FOUND\r\n");
-                send_string(socket, "Server: Tiny webserver\r\n\r\n");
-                send_string(socket, "<html><head><title>404 Not Found</title></head>");
-                send_string(socket, "<body><h1>URL not found</h1></body></html>\r\n");
-            } else {      // otherwise, serve up the file
-                printf(" 200 OK\n");
-                send_string(socket, "HTTP/1.0 200 OK\r\n");
-                send_string(socket, "Server: Tiny webserver\r\n\r\n");
-                if (ptr == request + 4) { // then this is a GET request
-                    if ((length = get_file_size(requested_file)) == -1) die("Getting resource file size");
-
-                    if ((ptr = (unsigned char *) malloc(length)) == NULL) die("Allocating memory for reading resource");
-
-                    read(requested_file, ptr, length); // read the file into memory
-                    send(socket, ptr, length, 0);  // send it to socket
-                    free(ptr); // free file memory
+        *ptr = 0; // reset pointer
+        ptr = NULL;
+        if (strncmp(request, "GET ", 4) == 0) ptr = request + 4; // checks if GET is present
+        if (strncmp(request, "HEAD ", 5) == 0) ptr = request + 5; // checks if HEAD is present
+        if (ptr == NULL) die("Unknown request"); // ptr was not moved by GET or HEAD
+        else { //ptr was moved, now points to requested file
+            if (ptr[strlen(ptr) - 1] == '/') strcat(ptr, "index.html");     // add 'index.html' to the end
+            strcpy(file_path, WEBROOT);
+            strcat(file_path, ptr);
+            requested_file = open(file_path, O_RDONLY, 0);
+            live("Opening requested file:\n%s\t %s%s\n", COLOR_POSITIVE, file_path, COLOR_NEUTRAL);
+            if (requested_file == -1) { // handle 404 not found
+                live("Response:\n%s\t %s%s\n", COLOR_NEGATIVE, "404 Not Found", COLOR_NEUTRAL);
+                send_string(socket, "HTTP/1.1 404 NOT FOUND\r\n\r\n");
+                send_string(socket, HTML404);
+            } else {
+                live("Response:\n%s\t %s%s\n", COLOR_POSITIVE, "200 OK", COLOR_NEUTRAL);
+                send_string(socket, "HTTP/1.0 200 OK\r\n\r\n");
+                if (ptr == request + 4) { // GET Request
+                    if ((file_size = get_file_size(requested_file)) == -1) die("Failed getting file size");
+                    if ((ptr = (unsigned char *) malloc(file_size)) == NULL)
+                        die("Failed allocating memory for reading");
+                    read(requested_file, ptr, file_size);
+                    send(socket, ptr, file_size, 0);
+                    free(ptr);
                 }
-                close(requested_file); // close the file
-            } // end if block for file found/not found
-        } // end if block for valid request
-    } // end if block for valid HTTP
-    shutdown(socket, SHUT_RDWR); // close the socket gracefully
+                close(requested_file);
+            }
+        }
+    }
+    shutdown(socket, SHUT_RDWR);
 }
 
 
-int get_file_size(int fd) {
-    struct stat stat_struct;
-
-    if (fstat(fd, &stat_struct) == -1)
-        return -1;
-    return (int) stat_struct.st_size;
-}
-
-
-/*  if ((value_read = read(socket, request, BUFFER_SIZE)) >= 0) {
-      live("Client request received:\n%s%s%s\n", COLOR_CLIENT_CONTENT, request,
-           COLOR_NEUTRAL);
-
-      const char *start_of_path = strchr(request, '/') + 1;
-      const char *end_of_path = strchr(request, 'H') - 1;
-      char path[end_of_path - start_of_path];
-      strncpy(path, start_of_path, end_of_path - start_of_path);
-      path[sizeof(path)] = 0;
-      live("Requested file:\n%s%s%s\n", COLOR_CLIENT_CONTENT, path, COLOR_NEUTRAL);
-
-      char *dot = strrchr(path, '.');
-      if (dot && !strcmp(dot, ".html")) { //HTML
-          server_response_header =
-                  "HTTP/1.1 200 OK\r\n"
-                  "Content-Type: text/html\r\n\n";
-      } else if (dot && !strcmp(dot, ".jpeg")) { //JPEG
-          server_response_header =
-                  "HTTP/1.1 200 OK\r\n"
-                  "Content-Type: image/jpeg\r\n\n";
-
-          live("RESPONSE: \n%s%s%s\n", COLOR_CLIENT_CONTENT, server_response_header, COLOR_NEUTRAL);
-      }
-
-
-
-      FILE *file = fopen(path, "rb");
-
-      if (file != NULL) {
-          fseek(file, 0, SEEK_END);
-          long file_size = ftell(file);
-          rewind(file);
-
-          char *file_content = (char *) malloc(file_size + 1);
-          fread(file_content, 1, file_size, file);
-          file_content[file_size] = 0;
-
-          fclose(file);
-
-          server_response = malloc(strlen(server_response_header) + strlen(file_content) + 1);
-          strcpy(server_response, server_response_header);
-          strcat(server_response, file_content);
-
-          free(file_content);
-
-
-          send_bytes = strlen(server_response);
-
-          if (write(socket, server_response, send_bytes) != send_bytes)
-              die("Writing server response failed");
-          else
-              live("Server response sent:\n%s%s%s", COLOR_SERVER_CONTENT, server_response, COLOR_NEUTRAL);
-
-          free(server_response);
-          free(socket_desc);
-          close(socket);
-
-      } else {
-          printf("%s[SERVER] ", COLOR_NEGATIVE);
-          printf("%s%s\n", COLOR_NEUTRAL, "No such file or directory, skipped");
-          free(socket_desc);
-
-          close(socket);
-      }
-
-  } else {
-      die("Reading client request failed");
-      free(socket_desc);
-
-      close(socket);
-  }*/
