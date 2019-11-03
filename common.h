b8eafe84a0ce41442aa851117a0f467879a53b41
Alexander Ameye <alexanderameye@gmail.com>
2019-11-03 21:53:36 +0900

    ad

--- common.h
+++ common.h
@@ -11,9 +11,10 @@
 #include <stdarg.h>
 #include <sys/stat.h>
 #include <time.h>
+
 /* SERVER */
-#define WEBROOT "./"
-#define SERVER_PORT 10080 // standard HTTP port
+#define WEB_ROOT "./"
+#define SERVER_PORT 10080
 #define BUFFER_SIZE 4096
 #define MAX_CONNECTIONS 100
 #define SOCKET_TYPE SOCK_STREAM // Connection oriented TCP Protocol
@@ -27,29 +28,28 @@
 #define LOGIN "username=alexanderameye&password=paswoord123"
 #define USERNAME "alexanderameye"
 #define COOKIE "Set-Cookie: logged_in=true; Expires="
-#define SESSION_TIME 20
+#define SESSION_TIME 30
 
 /* COLORS */
 #define COLOR_ACTION "\033[1;33m"
 #define COLOR_SERVER_CONTENT "\033[1;36m"
 #define COLOR_NEUTRAL "\033[0m"
 #define COLOR_NEGATIVE "\033[1;31m"
-#define COLOR_POSITIVE "\033[0;32m"
-#define COLOR_CLIENT_CONTENT "\033[1;35m"
+#define COLOR_POSITIVE "
 
+/* Read the first line of the sent request and returns it */
 int recv_line(int socket, unsigned char *request_header) {
-
-#define EOL "\r\n\r\n" // detects end of line, can be limited to \r\n
+#define EOL "\r\n\r\n"
 #define EOL_SIZE 4
-    unsigned char *ptr; // used to traverse the line
+    unsigned char *ptr;
     int eol_matched = 0;
     ptr = request_header;
-    while (recv(socket, ptr, 1, 0) == 1) { // read a single byte
-        if (*ptr == EOL[eol_matched]) { // check if byte matches and EOL character
+    while (recv(socket, ptr, 1, 0) == 1) {
+        if (*ptr == EOL[eol_matched]) {
             eol_matched++;
-            if (eol_matched == EOL_SIZE) { // check if all bytes match EOL
+            if (eol_matched == EOL_SIZE) {
                 *(ptr + 1 - EOL_SIZE) = '\0'; // terminate string
-                return strlen(request_header); // return received bytes
+                return strlen(request_header);
             }
         } else eol_matched = 0;
         ptr++;
@@ -57,8 +57,7 @@ int recv_line(int socket, unsigned char *request_header) {
     return 0;
 }
 
-//Expires=Wed, 21 Oct 2015 07:28:00 GMT
-//Expires=Mon, 4 Nov 2019 06:36:00 GMT
+/* Converts a tm struct to a string we can use for our cookie expiration time */
 char *time_string(const struct tm *timeptr) {
     static char wday_name[7][3] = {
             "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
@@ -70,21 +69,22 @@ char *time_string(const struct tm *timeptr) {
     static char result[31];
 
     sprintf(result, "%.3s, %d %.3s %d %.2d:%.2d:%.2d GMT",
-            wday_name[timeptr->tm_wday], //SUN
-            timeptr->tm_mday, //4
-            mon_name[timeptr->tm_mon], //NOV
-            1900 + timeptr->tm_year, //2019
-            timeptr->tm_hour, //HOUR
-            timeptr->tm_min, //MIN
-            timeptr->tm_sec); //ADD 30 SECONDS
+            wday_name[timeptr->tm_wday],
+            timeptr->tm_mday, /
+            mon_name[timeptr->tm_mon],
+            1900 + timeptr->tm_year,
+            timeptr->tm_hour,
+            timeptr->tm_min,
+            timeptr->tm_sec);
     return result;
 }
 
+/* Sends a string to the socket byte by byte */
 int send_string(int socket, unsigned char *buffer) {
     int sent;
     int bytes = strlen(buffer);
     while (bytes > 0) {
-        sent = send(socket, buffer, bytes, 0); // send bytes to socket
+        sent = send(socket, buffer, bytes, 0);
         if (sent == -1) return 0;
         bytes -= sent;
         buffer += sent;
@@ -92,12 +92,14 @@ int send_string(int socket, unsigned char *buffer) {
     return 1;
 }
 
+/* Get the file size */
 int get_file_size(int file) {
     struct stat stat_struct;
     if (fstat(file, &stat_struct) == -1) return -1;
     return (int) stat_struct.st_size;
 }
 
+/* Logs an error message to the terminal */
 void die(char *message, ...) {
     va_list ap;
     va_start(ap, message);
@@ -111,6 +113,7 @@ void die(char *message, ...) {
     exit(1);
 }
 
+/* Logs a success or neutral message to the terminal */
 void live(char *message, ...) {
     va_list ap;
     va_start(ap, message);
